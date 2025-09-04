4b6b489fa4370680dfe8b50e78ee5c89c0f169fc
Tom Xiong <xiongtom151@gmail.com>
2025-09-04 09:55:20 +0200

    #2 Création du serveur web en mode.j et ajout des packages

--- JeuxJS/app.js
+++ JeuxJS/app.js
@@ -1,3 +1,23 @@
 'use strict';
+/*  *********************** Serveur Web ***************************   */
+var port = 80;
+var express = require('express'); 
+var exp = express(); 
+exp.use(express.static(__dirname + '/www')); 
 
-console.log('TP CIEL');
+exp.get('/', function (req, res)
+{
+    console.log('Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+}); 
+
+exp.use(function (err, req, res, next)
+{
+        console.error(err.stack);
+        res.status(500).send('Erreur serveur express');
+}); 
+
+exp.listen(80, function ()
+{
+    console.log('Serveur en ecoute' + port);
+}); 
